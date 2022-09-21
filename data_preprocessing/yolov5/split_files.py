caf86362702a70753405244f17c703070cab991c
swywssaid <94416699+swywssaid@users.noreply.github.com>
2022-09-21 22:06:23 +0900

    Feat: Split files
    - divide by percentage

---
+++ data_preprocessing/yolov5/split_files.py
@@ -0,0 +1,4 @@
+import splitfolders
+
+splitfolders.ratio('C:/Users/ss/PycharmProjects/pythonProject/smartfarm/image/곤줄박이/input', output='C:/Users/ss/PycharmProjects/pythonProject/smartfarm/image/곤줄박이/output',
+                   seed=1337, ratio=(0.8, 0.1, 0.1))
