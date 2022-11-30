0e34fdfb821a048cbc69b8d3ddabced6e5bcf0c0
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-11-30 23:39:07 +0900

    feat: Create arduino water temperature

---
+++ arduino/Water_Temp/Water_Temp_2.ino
@@ -0,0 +1,29 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);  
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  sensors.begin();  // Start up the library
+  Serial.begin(115200);
+}
+
+void loop(void)
+{ 
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+
+  //print the temperature in Celsius
+  Serial.print("");
+  Serial.println(sensors.getTempCByIndex(0));
+ 
+  delay(1000);
+}
