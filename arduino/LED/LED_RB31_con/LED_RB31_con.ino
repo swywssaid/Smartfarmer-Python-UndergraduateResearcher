a672ae9ee91cbe61902b46393c0a19f2579eec82
ssaid <94416699+swywssaid@users.noreply.github.com>
2022-11-30 23:33:02 +0900

    feat: Create arduino led

---
+++ arduino/LED/LED_RB31_con/LED_RB31_con.ino
@@ -0,0 +1,67 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// Released under the GPLv3 license to match the rest of the
+// Adafruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN        6 // On Trinket or Gemma, suggest changing this to 1
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 36 // Popular NeoPixel ring size
+
+// When setting up the NeoPixel library, we tell it how many pixels,
+// and which pin to use to send signals. Note that for older NeoPixel
+// strips you might need to change the third parameter -- see the
+// strandtest example for more information on possible values.
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  Serial.begin(9600);
+  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
+  // Any other board, you can remove this part (but no harm leaving it):
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  // END of Trinket-specific code.
+  pinMode(PIN, OUTPUT);
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+}
+
+void loop() {
+  char ch = 50;
+  if(Serial.available()){
+    
+    ch= Serial.read();
+  } 
+  if(ch <= 40){
+    for(int i=0; i<NUMPIXELS; i++) { 
+   
+    if(i%4==0){ 
+      pixels.setPixelColor(i, pixels.Color(0, 0, 255)); 
+      pixels.show();} 
+      
+     else{
+       pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+       pixels.show();}
+       
+   
+   }    
+  }      
+  else{
+    for(int i=0; i<NUMPIXELS; i++) { 
+   
+    if(i%4==0){ 
+      pixels.setPixelColor(i, pixels.Color(0, 0, 0)); 
+      pixels.show();} 
+      
+     else{
+       pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+       pixels.show();}     
+   }    
+  }                
+}
